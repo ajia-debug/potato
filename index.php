90a670e056202d359cca23ae29428ab3ce46498e
ajia-debug <ajia34647@gmail.com>
2020-07-03 17:03:36 +0800

    crontab

--- index.php
+++ index.php
@@ -4,21 +4,52 @@ use Jsyqw\PotatoBot\Potato;
 use Jsyqw\PotatoBot\PotatoRequest;
 
 require_once 'vendor/autoload.php';
-$potato = new Potato('10275702:VdjeBGir5tZDYWLef930906b','D字一族');
+$potato = new Potato('10275575:E6D8mm6DmDqzXXGP0e145p6r','9蜜');
+$group = 12070345;
 PotatoRequest::initialize($potato);
-$reqData = new \Jsyqw\PotatoBot\Requests\ReqSendMessage();
-$reqData->chat_type = \Jsyqw\PotatoBot\Types\ChatType::PeerChat;
-$reqData->chat_id = 12103450;
-$reqData->text = date('Y-m-d H:i:s');
-$ret = PotatoRequest::sendTextMessage($reqData);
-print_r($ret);
 
-$reqData = new \Jsyqw\PotatoBot\Requests\ReqSendVideo();
-$reqData->chat_id = 12103450; //群组
-$reqData->chat_type = \Jsyqw\PotatoBot\Types\ChatType::PeerChat;
-$reqData->video = '0020022c9c416dc2cb5e63f9768e3a23';
-//$reqData->video = fopen('./mov_bbb.mp4', 'r');
-//$reqData->thumb = fopen('./img.png','r');
-$reqData->caption = '机器人自动发的视频';
-$ret = PotatoRequest::sendVideo($reqData);
-print_r($ret);
+$videoes = glob('./download/*.mp4');
+$alreadySend = file_get_contents('./send/'.$group.'.txt');
+$alreadySendArr = explode("\r\n",$alreadySend);
+$limitSend = 5;//限制每次发送的视频数量
+$i=1;
+
+$removeDate = date('Y-m-d',strtotime('-0 days'));
+//删除昨天保存的已发送视频记录
+$sendFileTxt = glob('./send/*');
+foreach($sendFileTxt as $txt){
+	echo $removeDate;
+	if(date('Y-m-d',filectime($txt)) == $removeDate){
+		unlink($txt);
+	}
+}
+foreach($videoes as $vi){
+	//删除两天前下载的视频
+	if(date('Y-m-d',filectime($vi)) == $removeDate){
+		unlink($vi);
+		continue;
+	}
+	if($i>$limitSend){
+		break;
+	}
+	
+	if(in_array(basename($vi),$alreadySendArr)){
+		echo $vi.' Already Send'.PHP_EOL;
+		continue;
+	}
+
+	$reqData = new \Jsyqw\PotatoBot\Requests\ReqSendVideo();
+	$reqData->chat_id = $group; //群组
+	$reqData->chat_type = 3;//\Jsyqw\PotatoBot\Types\ChatType::PeerChat;
+	$reqData->video = fopen($vi, 'r');
+	//$reqData->thumb = fopen('./img.png','r');
+	//$reqData->caption = '机器人自动发的视频';
+	
+	$ret = PotatoRequest::sendVideo($reqData);
+	
+	if($ret->ok == 1){
+		//登记发送成功的视频
+		file_put_contents('./send/'.$group.'.txt',basename($vi)."\r\n",FILE_APPEND);
+		$i++;
+	}
+}
---
+++ spider.php
@@ -0,0 +1,92 @@
+<?php
+// #母狗 #淫妻 #妓女 #群交 #滥交 #骚逼 #肉便器 #精液 #阿姨 #儿子 #干妈 #乱伦 #绿帽 #3P #无套 #内射 #换妻 #妈妈 #外婆 #奶奶 #孙子 #夫妻 #单男 #潮吹 #中出 #套套 #射精
+$search = array(
+	'骚逼',
+	'麻豆',
+	'91',
+	'丝袜',
+	'换妻',
+	'射精',
+	'国产自拍',
+	'韩国女主播'
+);
+$startTime = date('Y-m-d',strtotime('-2 days'));
+$endTime = date('Y-m-d',strtotime('-1 days'));
+
+foreach($search as $word){
+	$keywords = urlencode($word.' until:'.$endTime.' since:'.$startTime);
+
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+	  CURLOPT_URL => "https://api.twitter.com/1.1/search/tweets.json?q=".$keywords."&result_type=mixed&count=100",
+	  CURLOPT_RETURNTRANSFER => true,
+	  CURLOPT_SSL_VERIFYPEER =>false,
+	  CURLOPT_ENCODING => "",
+	  CURLOPT_MAXREDIRS => 10,
+	  CURLOPT_TIMEOUT => 0,
+	  CURLOPT_FOLLOWLOCATION => true,
+	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	  CURLOPT_CUSTOMREQUEST => "GET",
+	  CURLOPT_HTTPHEADER => array(
+		"authorization: Bearer AAAAAAAAAAAAAAAAAAAAAGV7FgEAAAAA4b7rzA4NQWLtepeGptx3N2Gfx5w%3Dq54iC6aXP9CMb9j99hfhTTSkI4o1JpOXFqeNVMF8KAX2eTuQSr",
+	  ),
+	));
+	$response = curl_exec($curl);
+	curl_close($curl);
+	$videoUrls = array();
+	//解析json
+	$response = json_decode($response,true);
+	if(!empty($response['statuses'])){
+		//遍历数据获取视频
+		foreach($response['statuses'] as $tweets){
+			//检查视频 $tweets['text'] 推文内容
+			//检查是否有视频资源内容
+			if(isset($tweets['extended_entities']['media'])){
+				//获取视频资源
+				foreach($tweets['extended_entities']['media'] as $media){
+					// duration_millis 视频秒数 过滤低于120秒视频
+					if(isset($media['video_info']['variants']) && $media['video_info']['duration_millis'] < 120000){
+						continue;
+					}
+					if(isset($media['video_info']['variants'])){
+						//遍历视频码率资源 优先固定 832000 的 mp4
+						$url = '';
+						$stardUrl = '';
+						foreach($media['video_info']['variants'] as $video){
+							if($video['content_type'] == 'video/mp4'){
+								$url = $video['url'];
+								if($video['bitrate'] == '832000'){
+									$stardUrl = $video['url'];
+									break;
+								}
+							}
+						}
+						if(!empty($stardUrl)){
+							$videoUrls[] = $stardUrl;
+						}elseif(!empty($url)){
+							$videoUrls[] = $url;
+						}
+					}
+				}
+			}
+		}
+	}
+	//视频资源去重
+	$videoUrls = array_unique($videoUrls);
+	//echo count($videoUrls).PHP_EOL;
+	//continue;
+	
+	//检查视频是否已经被下载过
+	foreach($videoUrls as $v){
+		$fileName = parse_url($v,PHP_URL_PATH);
+		$fileName = explode('/',$fileName);
+		$file = array_pop($fileName);
+		//检查视频是否存在
+		if(!file_exists('./download/'.$file)){
+			$flag = file_put_contents('./download/'.$file,file_get_contents($v));
+			if($flag !== false){
+				//记录下载成功到视频
+			}
+		}
+	}
+}
